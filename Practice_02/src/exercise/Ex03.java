5c02354532fc68ec25ea1e8d8c79ef49b5986ad8
Soojung Kwak <skwak0205@gmail.com>
2021-05-29 16:40:20 +0900

    02. 기본 자료구조

---
+++ Practice_02/src/exercise/Ex03.java
@@ -0,0 +1,33 @@
+package exercise;
+
+import java.util.Scanner;
+
+public class Ex03 {
+	static int sumOf(int[] a) {
+		int sum = 0;
+		
+		for(int i = 0; i < a.length; i++) {
+			sum += a[i];
+		}
+		
+		
+		return sum;
+	}
+	
+	
+	public static void main(String[] args) {
+		Scanner stdIn = new Scanner(System.in);
+
+		System.out.print("요솟수는：");
+		int num = stdIn.nextInt();	 // 요솟수
+
+		int[] a = new int[num]; 	// 길이가 num인 배열
+
+		for (int i = 0; i < num; i++) {
+			System.out.print("a[" + i + "] : ");
+			a[i] = stdIn.nextInt();
+		}
+
+		System.out.println("전 요소의 합계는 " + sumOf(a) + "입니다.");
+	}
+}
---
+++ Practice_02/src/practice/CloneArray.java
@@ -0,0 +1,21 @@
+package practice;
+
+public class CloneArray {
+	public static void main(String[] args) {
+		int[] a = {1, 2, 3, 4, 5};
+		int[] b = a.clone();  // a의 복제를 참조
+		
+		b[3] = 0;
+		
+		System.out.print("a =");
+		for(int i = 0; i < a.length; i++) {
+			System.out.print(" " + a[i]);
+		}
+		
+		System.out.print("\nb =");
+		for(int i = 0; i < b.length; i++) {
+			System.out.print(" " + b[i]);
+		}
+		
+ 	}
+}
